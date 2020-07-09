280991a89fdff84b54b7ab2a2ff2adff08d4c794
David Ruda <davidrth@seznam.cz>
2020-07-09 23:53:59 +0200

    Create bomb spawner

---
+++ Bomberman/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public GameObject bombPrefab;
+
+    public GameObject player;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 worldPos = player.transform.position;
+            Vector3Int cell = tilemap.WorldToCell(worldPos);
+            Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+            Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+        }
+    }
+}
